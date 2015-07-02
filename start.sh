056cd0676967066bde4cd0fe6c0a846374c6aa72
James Rawlings <jamesrawlings@rely.eclipse.co.uk>
2015-07-02 17:07:41 +0100

    switched to use fabric8/hubot-base image

--- start.sh
+++ start.sh
@@ -1,22 +1,8 @@
 #!/bin/sh -u
 
-function clone_copy_cleanup(){
-  # clone repo and cd to git repo but removing the '.git' from the directory name
-  dirname=$(basename $1)
-  len=${#dirname}-4
-
-  cd /tmp && git clone $1 && cd $(echo "${dirname:0:$len}")
-
-  # Move coffeescripts to Hubot scripts folder so they are installed at startup
-  mv *.coffee /home/hubot/scripts
-
-  rm -rf /tmp/$(echo "${dirname:0:$len}")
-}
-
 export HUBOT_LCB_HOSTNAME=$LETSCHAT_SERVICE_HOST
 export HUBOT_LCB_PORT=$LETSCHAT_SERVICE_PORT
 
-clone_copy_cleanup $LETSCHAT_HUBOT_SCRIPTS
 cd /home/hubot
 
 AUTH_HEADER=`echo $LETSCHAT_HUBOT_USERNAME:$LETSCHAT_HUBOT_PASSWORD | base64`
