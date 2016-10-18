efde7ff1cead62a7fed3363b39a608aa73ebfe21
rawlingsj <rawlingsj80@gmail.com>
2016-10-18 22:41:24 +0100

    export authtoken so hubot can connect to letschat

--- start.sh
+++ start.sh
@@ -1,27 +1,23 @@
 #!/bin/sh -u
 
-export HUBOT_LCB_HOSTNAME=$LETSCHAT_SERVICE_HOST
-export HUBOT_LCB_PORT=$LETSCHAT_SERVICE_PORT
-
-export HUBOT_LOG_LEVEL="debug"
+export HUBOT_LCB_HOSTNAME=letschat
+export HUBOT_LCB_PORT=80
 
 cd /home/hubot
 
 AUTH_HEADER=`echo $LETSCHAT_HUBOT_USERNAME:$LETSCHAT_HUBOT_PASSWORD | base64`
-
-HUBOT_LCB_TOKEN=""
+BEARER_TOKEN=""
 
 # Poll every 5 seconds until letschat is up and generates a token
-while [ -z "$HUBOT_LCB_TOKEN" ]
+while [ -z "$BEARER_TOKEN" ]
 do
     echo "Let's Chat token is empty; lets query again in 5 seconds...."
     sleep 5
 
     JSON=`curl --silent -X POST  -H "Accept: application/json" -H "Content-type: application/json" -H "Authorization: Basic ${AUTH_HEADER}" -d "{}" http://${LETSCHAT_SERVICE_HOST}:${LETSCHAT_SERVICE_PORT}/account/token/generate`
-    HUBOT_LCB_TOKEN=`echo $JSON | jq '.token'`
+    echo $JSON
+    BEARER_TOKEN=`echo $JSON | jq '.token'`
 
-    echo "Let's Chat token is: $HUBOT_LCB_TOKEN"
 done
-
-
+export HUBOT_LCB_TOKEN=$BEARER_TOKEN
 bin/hubot -a lets-chat
